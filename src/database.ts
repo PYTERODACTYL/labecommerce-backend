ac59e7d3fdaf083f0e55bb9ef68c34ac4fa9cf3f
PYTERODACTYL <pyter512@live.com>
2023-01-19 13:28:19 -0300

    segundo commit

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { Product, Purchase, User } from "./types";
+import { CATEGORIAS, Product, Purchase, User } from "./types";
 
 export const users: User[] = [
     {
@@ -17,13 +17,13 @@ export const products: Product[]=[{
     id: "1",
     name: "banana",
     price: 10,
-    category: "fruta"
+    category: CATEGORIAS.FRUITS
 },
 {
     id: "2",
     name: "batata",
     price: 5,
-    category: "legume"
+    category: CATEGORIAS.FRUITS
 }
 ]
 
@@ -38,3 +38,48 @@ export const purchases: Purchase[]=[{
     quantity: 5,
     totalPrice: 50
 }]
+
+export function createUser(id:string,email:string,password:string){
+    return console.log("Cadastro realizado com sucesso");
+    
+}
+
+export function getAllUsers(){
+    return console.log(users);
+    
+}
+
+export function createProduct(id:string,name:string,price:number,category:CATEGORIAS){
+    return console.log("Produto criado com sucesso");
+    
+}
+
+export function getAllProducts(){
+    return console.log(products);
+    
+}
+
+export function getProductById(idToSearch:string){
+    for(let i=0;i<products.length;i++){
+        if(idToSearch==products[i].id){
+            return products[i]
+        }
+        return console.log("undefined");
+        
+    }
+}
+
+export function queryProductsByName(q:any){
+    return console.log(products);
+    
+}
+
+export function createPurchase(userId:string,productId:string,quantity:number,totalPrice:number){
+    console.log("Compra realizada com sucesso");
+    
+}
+
+export function getAllPurchasesFromUserId(userIdToSearch:string){
+    console.log("sucessoo");
+    
+}
